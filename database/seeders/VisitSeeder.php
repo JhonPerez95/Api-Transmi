12d09815d194a73bc2330bbae6c39ef9e1aae41c
jhonperez95 <jhonperez.j95@gmail.com>
2022-01-17 01:41:45 -0500

    se corrige migracion

--- database/seeders/VisitSeeder.php
+++ database/seeders/VisitSeeder.php
@@ -18,7 +18,7 @@ class VisitSeeder extends Seeder
         $data = Visit::create([
             'parkings_id'  => '1',
             'number'  => '1',
-            'bikers_id'  => '1',
+            'bikers_id'  => '3',
             'bicies_id'  => '1',
             'duration'  => 60,
             'date_input'  => '2020-12-20',
@@ -31,7 +31,7 @@ class VisitSeeder extends Seeder
         $data = Visit::create([
             'parkings_id'  => '2',
             'number'  => '1',
-            'bikers_id'  => '1',
+            'bikers_id'  => '3',
             'bicies_id'  => '2',
             'duration'  => 86400,
             'date_input'  => '2020-12-20',
@@ -46,7 +46,7 @@ class VisitSeeder extends Seeder
         $data = Visit::create([
             'parkings_id'  => '3',
             'number'  => '1',
-            'bikers_id'  => '2',
+            'bikers_id'  => '3',
             'bicies_id'  => '3',
             'duration'  => 60,
             'date_input'  => '2020-12-20',
@@ -59,7 +59,7 @@ class VisitSeeder extends Seeder
         $data = Visit::create([
             'parkings_id'  => '4',
             'number'  => '1',
-            'bikers_id'  => '2',
+            'bikers_id'  => '3',
             'bicies_id'  => '4',
             'duration'  => 86400,
             'date_input'  => '2020-12-20',
@@ -87,7 +87,7 @@ class VisitSeeder extends Seeder
         $data = Visit::create([
             'parkings_id'  => '2',
             'number'  => '3',
-            'bikers_id'  => '6',
+            'bikers_id'  => '3',
             'bicies_id'  => '4',
             'duration'  => 86400,
             'date_input'  => '2020-12-20',
@@ -102,7 +102,7 @@ class VisitSeeder extends Seeder
         $data = Visit::create([
             'parkings_id'  => '3',
             'number'  => '2',
-            'bikers_id'  => '4',
+            'bikers_id'  => '3',
             'bicies_id'  => '7',
             'duration'  => 60,
             'date_input'  => '2020-12-20',
@@ -115,7 +115,7 @@ class VisitSeeder extends Seeder
         $data = Visit::create([
             'parkings_id'  => '3',
             'number'  => '3',
-            'bikers_id'  => '4',
+            'bikers_id'  => '3',
             'bicies_id'  => '8',
             'duration'  => 86400,
             'date_input'  => '2020-12-20',
@@ -130,7 +130,7 @@ class VisitSeeder extends Seeder
         $data = Visit::create([
             'parkings_id'  => '4',
             'number'  => '2',
-            'bikers_id'  => '5',
+            'bikers_id'  => '3',
             'bicies_id'  => '9',
             'duration'  => 60,
             'date_input'  => '2020-12-20',
@@ -143,7 +143,7 @@ class VisitSeeder extends Seeder
         $data = Visit::create([
             'parkings_id'  => '4',
             'number'  => '3',
-            'bikers_id'  => '5',
+            'bikers_id'  => '3',
             'bicies_id'  => '10',
             'duration'  => 86400,
             'date_input'  => '2020-12-20',
@@ -157,7 +157,7 @@ class VisitSeeder extends Seeder
         $data = Visit::create([
             'parkings_id'  => '1',
             'number'  => '2',
-            'bikers_id'  => '6',
+            'bikers_id'  => '3',
             'bicies_id'  => '11',
             'duration'  => 60,
             'date_input'  => '2020-12-20',
@@ -170,7 +170,7 @@ class VisitSeeder extends Seeder
         $data = Visit::create([
             'parkings_id'  => '1',
             'number'  => '3',
-            'bikers_id'  => '6',
+            'bikers_id'  => '3',
             'bicies_id'  => '12',
             'duration'  => 86400,
             'date_input'  => '2020-12-20',
@@ -186,7 +186,7 @@ class VisitSeeder extends Seeder
         $data = Visit::create([
             'parkings_id'  => '1',
             'number'  => '1',
-            'bikers_id'  => '1',
+            'bikers_id'  => '3',
             'bicies_id'  => '1',
             'duration'  => 60,
             'date_input'  => '2020-12-21',
@@ -199,7 +199,7 @@ class VisitSeeder extends Seeder
         $data = Visit::create([
             'parkings_id'  => '3',
             'number'  => '1',
-            'bikers_id'  => '2',
+            'bikers_id'  => '3',
             'bicies_id'  => '3',
             'duration'  => 60,
             'date_input'  => '2020-12-21',
@@ -222,36 +222,11 @@ class VisitSeeder extends Seeder
             //'new'  => 'Ninguna',
             'visit_statuses_id'  => '1',
         ]);
-        $data = Visit::create([
-            'parkings_id'  => '3',
-            'number'  => '2',
-            'bikers_id'  => '4',
-            'bicies_id'  => '7',
-            'duration'  => 60,
-            'date_input'  => '2020-12-21',
-            'time_input'  => '02:02:00',
-            'date_output'  => '2020-12-21',
-            'time_output'  => '02:03:00',
-            //'new'  => 'Ninguna',
-            'visit_statuses_id'  => '1',
-        ]);
-        $data = Visit::create([
-            'parkings_id'  => '4',
-            'number'  => '1',
-            'bikers_id'  => '5',
-            'bicies_id'  => '9',
-            'duration'  => 60,
-            'date_input'  => '2020-12-21',
-            'time_input'  => '02:02:00',
-            'date_output'  => '2020-12-21',
-            'time_output'  => '02:03:00',
-            //'new'  => 'Ninguna',
-            'visit_statuses_id'  => '1',
-        ]);
+
         $data = Visit::create([
             'parkings_id'  => '1',
             'number'  => '2',
-            'bikers_id'  => '6',
+            'bikers_id'  => '3',
             'bicies_id'  => '11',
             'duration'  => 60,
             'date_input'  => '2020-12-21',
@@ -267,7 +242,7 @@ class VisitSeeder extends Seeder
         $data = Visit::create([
             'parkings_id'  => '1',
             'number'  => '1',
-            'bikers_id'  => '1',
+            'bikers_id'  => '3',
             'bicies_id'  => '1',
             'duration'  => 600,
             'date_input'  => '2020-12-22',
@@ -280,7 +255,7 @@ class VisitSeeder extends Seeder
         $data = Visit::create([
             'parkings_id'  => '3',
             'number'  => '1',
-            'bikers_id'  => '2',
+            'bikers_id'  => '3',
             'bicies_id'  => '3',
             'duration'  => 600,
             'date_input'  => '2020-12-22',
@@ -303,203 +278,5 @@ class VisitSeeder extends Seeder
             //'new'  => 'Ninguna',
             'visit_statuses_id'  => '1',
         ]);
-        $data = Visit::create([
-            'parkings_id'  => '3',
-            'number'  => '2',
-            'bikers_id'  => '4',
-            'bicies_id'  => '7',
-            'duration'  => 600,
-            'date_input'  => '2020-12-22',
-            'time_input'  => '02:02:00',
-            'date_output'  => '2020-12-22',
-            'time_output'  => '02:12:00',
-            //'new'  => 'Ninguna',
-            'visit_statuses_id'  => '1',
-        ]);
-        $data = Visit::create([
-            'parkings_id'  => '4',
-            'number'  => '1',
-            'bikers_id'  => '5',
-            'bicies_id'  => '9',
-            'duration'  => 600,
-            'date_input'  => '2020-12-22',
-            'time_input'  => '02:02:00',
-            'date_output'  => '2020-12-22',
-            'time_output'  => '02:12:00',
-            //'new'  => 'Ninguna',
-            'visit_statuses_id'  => '1',
-        ]);
-        $data = Visit::create([
-            'parkings_id'  => '1',
-            'number'  => '2',
-            'bikers_id'  => '6',
-            'bicies_id'  => '11',
-            'duration'  => 600,
-            'date_input'  => '2020-12-22',
-            'time_input'  => '02:02:00',
-            'date_output'  => '2020-12-22',
-            'time_output'  => '02:12:00',
-            //'new'  => 'Ninguna',
-            'visit_statuses_id'  => '1',
-        ]);
-
-
-
-
-
-
-
-         $data = Visit::create([
-            'parkings_id'  => '1',
-            'number'  => '1',
-            'bikers_id'  => '1',
-            'bicies_id'  => '1',
-            'duration'  => 600,
-            'date_input'  => '2020-12-23',
-            'time_input'  => '02:02:00',
-            'date_output'  => '2020-12-23',
-            'time_output'  => '02:12:00',
-            //'new'  => 'Ninguna',
-            'visit_statuses_id'  => '1',
-        ]);
-        $data = Visit::create([
-            'parkings_id'  => '3',
-            'number'  => '1',
-            'bikers_id'  => '2',
-            'bicies_id'  => '3',
-            'duration'  => 600,
-            'date_input'  => '2020-12-23',
-            'time_input'  => '02:02:00',
-            'date_output'  => '2020-12-23',
-            'time_output'  => '02:12:00',
-            //'new'  => 'Ninguna',
-            'visit_statuses_id'  => '1',
-        ]);
-        $data = Visit::create([
-            'parkings_id'  => '2',
-            'number'  => '1',
-            'bikers_id'  => '3',
-            'bicies_id'  => '5',
-            'duration'  => 600,
-            'date_input'  => '2020-12-23',
-            'time_input'  => '02:02:00',
-            'date_output'  => '2020-12-23',
-            'time_output'  => '02:12:00',
-            //'new'  => 'Ninguna',
-            'visit_statuses_id'  => '1',
-        ]);
-
-
-
-
-
-        $data = Visit::create([
-            'parkings_id'  => '4',
-            'number'  => '1',
-            'bikers_id'  => '5',
-            'bicies_id'  => '9',
-            'duration'  => 600,
-            'date_input'  => '2020-12-24',
-            'time_input'  => '01:02:00',
-            'date_output'  => '2020-12-24',
-            'time_output'  => '01:12:00',
-            //'new'  => 'Ninguna',
-            'visit_statuses_id'  => '1',
-        ]);
-        $data = Visit::create([
-            'parkings_id'  => '1',
-            'number'  => '1',
-            'bikers_id'  => '6',
-            'bicies_id'  => '11',
-            'duration'  => 600,
-            'date_input'  => '2020-12-24',
-            'time_input'  => '01:02:00',
-            'date_output'  => '2020-12-24',
-            'time_output'  => '01:12:00',
-            //'new'  => 'Ninguna',
-            'visit_statuses_id'  => '1',
-        ]);
-         $data = Visit::create([
-            'parkings_id'  => '4',
-            'number'  => '2',
-            'bikers_id'  => '5',
-            'bicies_id'  => '9',
-            'duration'  => 600,
-            'date_input'  => '2020-12-24',
-            'time_input'  => '02:02:00',
-            'date_output'  => '2020-12-24',
-            'time_output'  => '02:12:00',
-            //'new'  => 'Ninguna',
-            'visit_statuses_id'  => '1',
-        ]);
-        $data = Visit::create([
-            'parkings_id'  => '1',
-            'number'  => '2',
-            'bikers_id'  => '6',
-            'bicies_id'  => '11',
-            'duration'  => 600,
-            'date_input'  => '2020-12-24',
-            'time_input'  => '02:02:00',
-            'date_output'  => '2020-12-24',
-            'time_output'  => '02:12:00',
-            //'new'  => 'Ninguna',
-            'visit_statuses_id'  => '1',
-        ]);
-         $data = Visit::create([
-            'parkings_id'  => '4',
-            'number'  => '3',
-            'bikers_id'  => '5',
-            'bicies_id'  => '9',
-            'duration'  => 600,
-            'date_input'  => '2020-12-24',
-            'time_input'  => '03:02:00',
-            'date_output'  => '2020-12-24',
-            'time_output'  => '03:12:00',
-            //'new'  => 'Ninguna',
-            'visit_statuses_id'  => '1',
-        ]);
-        $data = Visit::create([
-            'parkings_id'  => '1',
-            'number'  => '3',
-            'bikers_id'  => '6',
-            'bicies_id'  => '11',
-            'duration'  => 86400 + 600,
-            'date_input'  => '2020-12-24',
-            'time_input'  => '03:02:00',
-            'date_output'  => '2020-12-25',
-            'time_output'  => '03:12:00',
-            //'new'  => 'Ninguna',
-            'visit_statuses_id'  => '1',
-        ]);
-
-
-        $data = Visit::create([
-            'parkings_id'  => '1',
-            'number'  => '3',
-            'bikers_id'  => '7',
-            'bicies_id'  => '13',
-            'duration'  => 0,
-            'date_input'  => '2020-02-24',
-            'time_input'  => '03:02:00',
-            'date_output'  => '2020-02-24',
-            'time_output'  => '03:02:00',
-            //'new'  => 'Ninguna',
-            'visit_statuses_id'  => '1',
-        ]);
-
-        $data = Visit::create([
-            'parkings_id'  => '2',
-            'number'  => '3',
-            'bikers_id'  => '7',
-            'bicies_id'  => '14',
-            'duration'  => 0,
-            'date_input'  => '2020-08-24',
-            'time_input'  => '03:02:00',
-            'date_output'  => '2020-02-24',
-            'time_output'  => '03:02:00',
-            //'new'  => 'Ninguna',
-            'visit_statuses_id'  => '1',
-        ]);
-
     }
 }
